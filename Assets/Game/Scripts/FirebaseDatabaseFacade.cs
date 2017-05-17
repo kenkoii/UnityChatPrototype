2e68445c21b112f3c828c16b3544ed1d465fd974
jktarban <pantoy@joshuas-MacBook-Pro.local>
2017-05-17 09:35:07 +0800

    First commit

---
+++ Assets/Game/Scripts/FirebaseDatabaseFacade.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+
+public class FirebaseDatabaseFacade : SingletonMonoBehaviour<FirebaseDatabaseFacade> {
+
+	void Start() {
+		// Set this before calling into the realtime database.
+		FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://chatprototype-39807.firebaseio.com");
+		DatabaseReference reference = FirebaseDatabase.DefaultInstance.RootReference;
+	}
+
+
+}
