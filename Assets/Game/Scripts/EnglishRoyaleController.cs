5cd8a88ee045a1388f2c815b2693d38ddc6a6597
jktarban <pantoy@joshuas-MacBook-Pro.local>
2017-06-19 14:16:08 +0800

    Initial mode 4

--- Assets/Game/Scripts/EnglishRoyaleController.cs
+++ Assets/Game/Scripts/EnglishRoyaleController.cs
@@ -3,9 +3,9 @@
 	public QuestionController questionController;
 	public BattleController battleController;
 	public LobbyController lobbyController;
-	public Phase1Controller phase1Controller;
-	public Phase2Controller phase2Controller;
-	public Phase3Controller phase3Controller;
+	public PhaseAnswerController phaseAnswerController;
+	public PhaseSkillController phaseSkillController;
+	public PhaseAttackController phaseAttackController;
 	public Skill1Controller skill1Controller;
 	public Skill2Controller skill2Controller;
 	public AudioController audioController;
