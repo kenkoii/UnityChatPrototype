f761b65bdac5060cfc690d8ca7a20f2792273554
jktarban <pantoy@joshuas-MacBook-Pro.local>
2017-06-27 15:05:48 +0800

    Added loading ui

--- Assets/Game/Scripts/TweenModel.cs
+++ Assets/Game/Scripts/TweenModel.cs
@@ -6,4 +6,12 @@ public class TweenModel : EnglishRoyaleElement {
 	public Slider playerGpSlider;
 	public Slider playerHpSlider;
 	public Slider enemyHpSlider;
+
+	public Transform playerHPGroup;
+	public Transform playerGPGroup;
+	public Transform enemyHPGroup;
+
+	public Transform loadingIndicator;
+
+	public Transform waitOpponentGroup;
 }
